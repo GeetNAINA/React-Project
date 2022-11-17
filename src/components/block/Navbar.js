c7d8fb73b52d74a8535c2cea7b51c8c270c7ab04
geetNaina <gp@esokia-webagency.com>
2022-11-17 10:07:03 +0400

    Check

--- src/components/block/Navbar.js
+++ src/components/block/Navbar.js
@@ -7,33 +7,33 @@ function Navbar() {
     <div className="container">
     <div className="row">
     <div className="col-md-12">
-<nav class="navbar navbar-expand-lg">
-  <div class="container-fluid">
-  <Link to="/" class="navbar-brand">LOGO</Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+<nav className="navbar navbar-expand-lg">
+  <div className="container-fluid">
+  <Link to="/" className="navbar-brand">LOGO</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-            <Link to="/" class="nav-link active">Home</Link>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+            <Link to="/" className="nav-link active">Home</Link>
         </li>
-        <li class="nav-item">
-        <Link to="/about" class="nav-link">About Us</Link>
+        <li className="nav-item">
+        <Link to="/about" className="nav-link">About Us</Link>
         </li>
-        <li class="nav-item">
-        <Link to="/contact" class="nav-link">Contact</Link>
+        <li className="nav-item">
+        <Link to="/contact" className="nav-link">Contact</Link>
         </li>
-        <li class="nav-item">
-        <Link to="/oddEven" class="nav-link">OddEven</Link>
+        <li className="nav-item">
+        <Link to="/oddEven" className="nav-link">OddEven</Link>
         </li>
-        <li class="nav-item">
-        <Link to="/fetchdata" class="nav-link">FetchData</Link>
+        <li className="nav-item">
+        <Link to="/fetchdata" className="nav-link">FetchData</Link>
         </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button class="btn btn-outline-success" type="submit">Search</button>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button className="btn btn-outline-success" type="submit">Search</button>
       </form>
     </div>
   </div>
