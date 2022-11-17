2ad5554d5fd05599650a67f3f81a75ce10cc672e
Rak <hfr@esokia.com>
2022-11-17 09:31:35 +0400

    review

--- src/components/pages/FetchData.js
+++ src/components/pages/FetchData.js
@@ -1,113 +1,105 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function FetchData() {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get(
+        'https://bat-recup-staging-backend.cleverapps.io/api/current-annoucement' // create an .env file to store the api url
+      )
+      .then((res) => {
+        console.log(res.data.results);
+        setPosts(res.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    useEffect(() => {
-        axios
-            .get("https://bat-recup-staging-backend.cleverapps.io/api/current-annoucement")
-            .then((res) => {
-                console.log(res.data.results);
-                setPosts(res.data.results);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
+  return (
+    <div>
+      <ul>
+        {posts.map((post, index) => (
+          <li>
+            <div>
+              <h1>
+                {post.id} {post.title}{' '}
+              </h1>
+              <p>{post.presentation}</p>
+              <p>{post.status_subscription} </p>
 
-    }, []);
+              <p>
+                {post.start_date_subscription}{' '}
+                <span>{post.end_date_subscription} </span>
+              </p>
+              <p>{post.social_link_fb}</p>
+              <p> {post.social_link_insta} </p>
+              <p>{post.web_site_url}</p>
+            </div>
 
+            <div>
+              <p>{post.logo.id}</p>
+              <img src={post.logo.url} alt="logo"></img>
+            </div>
 
-    return (
-        <div>
-            <ul>
-                {posts.map((post, index) => (
-                    <li>
-                        <div>
-                            <h1>{post.id} {post.title} </h1>
-                            <p>{post.presentation}</p>
-                            <p>{post.status_subscription} </p>
+            {post.photos.map((photo, index) => {
+              return (
+                <div key={index}>
+                  <p>{photo.id}</p>
+                  <img src={photo.url} alt="photo" width={500}></img>{' '}
+                  {/*Redundant alt attribute. Screen-readers already announce `img` tags as an image. You don’t need to use the words `image`, `photo,` or `picture` (or any specified custom words) in the alt prop*/}
+                </div>
+              );
+            })}
 
-                            <p>{post.start_date_subscription} <span>{post.end_date_subscription} </span></p>
-                            <p>{post.social_link_fb}</p>
-                            <p>  {post.social_link_insta} </p>
-                            <p>{post.web_site_url}</p>
-                        </div>
+            <div>
+              <p>{post.subscription.id}</p>
+              <h2>{post.subscription.subscription_name}</h2>
+            </div>
 
-                        <div>
-                            <p>{post.logo.id}</p>
-                            <img src={post.logo.url} alt="logo"></img>
-                        </div>
+            <div>
+              <p>{post.subscription_limite}</p>
+            </div>
 
-                  {post.photos.map((photo, index) => {
-                    return(
-                        <div key={index}>
-                        <p>{photo.id}</p>
-                        <img src={photo.url} alt="photo" width={500}></img>
-                      
-                    </div>
-                    );
-                  })}
+            <div>
+              {post.activities.map((activity, index) => {
+                return (
+                  <div key={index}>
+                    <p>{activity.id}</p>
+                    <p>{activity.name}</p>
+                  </div>
+                );
+              })}
+            </div>
 
-                        
-                        <div>
-                            <p>{post.subscription.id}</p>
-                            <h2>{post.subscription.subscription_name}</h2>
-                        </div>
+            <div>
+              <p>{post.thematique.id}</p>
+              <p>{post.thematique.name}</p>
+            </div>
 
-                        <div>
-                            <p>{post.subscription_limite}</p>
-                        </div>
+            <div>
+              <p>{post.adress.id}</p>
+              <p>{post.adress.line1}</p>
+              <p>{post.adress.line2}</p>
+              <p>{post.adress.city}</p>
+              <p>{post.adress.postalCode}</p>
+              <p>{post.adress.countryCode}</p>
+              <p>{post.adress.countryCode}</p>
+            </div>
 
-                        <div>
-                            
-                  {post.activities.map((activity, index) => {
-                    return(
-                        <div key={index}>
-                        <p>{activity.id}</p>
-                        <p>{activity.name}</p>
-                      
-                    </div>
-                    );
-                  })}
-                        </div>
-
-                        <div>
-                            <p>{post.thematique.id}</p>
-                            <p>{post.thematique.name}</p>
-
-                        </div>
-
-                        <div>
-                        <p>{post.adress.id}</p> 
-                        <p>{post.adress.line1}</p> 
-                        <p>{post.adress.line2}</p> 
-                        <p>{post.adress.city}</p> 
-                        <p>{post.adress.postalCode}</p> 
-                        <p>{post.adress.countryCode}</p> 
-                        <p>{post.adress.countryCode}</p>
-                        </div>
-
-                        <div>
-                            <p>{post.adress.location.id}</p>
-                            <p>{post.adress.location.type}</p>
-                            <p>{post.adress.location.lat}</p>
-                            <p>{post.adress.location.lng}</p>
-
-                        </div>
-
-                    </li>
-
-                ))
-                }
-            </ul>
-        </div>
-
-    );
-        
+            <div>
+              <p>{post.adress.location.id}</p>
+              <p>{post.adress.location.type}</p>
+              <p>{post.adress.location.lat}</p>
+              <p>{post.adress.location.lng}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-
-
 
 export default FetchData;
