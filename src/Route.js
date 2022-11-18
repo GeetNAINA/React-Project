4b31d1e1fdeb3d38c38877b3d2e5620d1f5c83a1
Rak <hfr@esokia.com>
2022-11-18 15:09:38 +0400

    review

--- src/Route.js
+++ src/Route.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Contact from './components/pages/Contact';
+import Home from './components/pages/Home';
 // import Error from './components/pages/Error';
-import OddEven from './components/pages/OddEven';
-import FetchData from './components/pages/FetchData';
-import Dynamic from './components/pages/Dynamic';
 import { Route, Routes } from 'react-router-dom';
 import Annoucement from './components/pages/Annoucement';
+import Dynamic from './components/pages/Dynamic';
+import FetchData from './components/pages/FetchData';
+import OddEven from './components/pages/OddEven';
 
 function Routing() {
   return (
     <Routes>
-      <Route path="/" element={ <Home />}/>
-      <Route path="/about" element={<About />}/>
-      <Route path="/contact" element={<Contact />}/>
-      <Route path="/oddEven" element={<OddEven />}/>
-      <Route path="/fetchdata" element={<FetchData />}/>
-      <Route path="/dynamic" element={<Dynamic />}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/oddEven" element={<OddEven />} />
+      <Route path="/fetchdata" element={<FetchData />} />
+      <Route path="/dynamic" element={<Dynamic />} />
       <Route path="/annoucement/:id" element={<Annoucement />} />
       {/* <Route path="/*" element={<Error />}/> */}
     </Routes>
--- src/components/pages/FetchData.js
+++ src/components/pages/FetchData.js
@@ -3,14 +3,14 @@ import React, { useEffect, useState } from 'react';
 
 function FetchData() {
   const [posts, setPosts] = useState([]);
-  // useEffect allows to perform effects: this allows our component to perform actions after display, 
+  // useEffect allows to perform effects: this allows our component to perform actions after display,
   //by choosing when this action should be performed.
   // The useEffect hook is called after each rendering of your component.
   // They allow us to perform an action at a given moment in the life cycle of our components.
   useEffect(() => {
-    // So here, as soon as the user arrives on your fetchData page, 
-    //you will call the dynamic data from the api and once the api does not return 
-    //any errors you update the states to store the JSON return WHICH YOU ARE GOING TO DISPLAY 
+    // So here, as soon as the user arrives on your fetchData page,
+    //you will call the dynamic data from the api and once the api does not return
+    //any errors you update the states to store the JSON return WHICH YOU ARE GOING TO DISPLAY
     //after calling the api all of this happens before the page is properly rendered
     axios
       .get(
@@ -25,16 +25,17 @@ function FetchData() {
       });
   }, []);
 
-
-    return (
-        <div>
-            <ul>
-                {posts.map((post) => (
-                    <li key={post.id}>
-                        <div>
-                            <h1>{post.id} {post.title} </h1>
-                            <p>{post.presentation}</p>
-                            <p>{post.status_subscription} </p>
+  return (
+    <div>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <div>
+              <h1>
+                {post.id} {post.title}{' '}
+              </h1>
+              <p>{post.presentation}</p>
+              <p>{post.status_subscription} </p>
 
               <p>
                 {post.start_date_subscription}{' '}
@@ -45,8 +46,7 @@ function FetchData() {
               <p>{post.web_site_url}</p>
             </div>
 
-
-  {/*If you know the data type array , here we make a loop */}
+            {/*If you know the data type array , here we make a loop */}
             {/*There are several ways to do the loop, that is to say browse the elements of the object array but Javascript there is already a powerful native function that will allow you to do this easily and that is the .MAP*/}
             {/*you can use foreach or use the for classic loop
               ex: for(i=0; i<data.length ; i++) {
@@ -55,26 +55,29 @@ function FetchData() {
 
             {/*So thanks to the loop, you will write less code to create tags. You need to add a KEY for each dynamically created tag, this will remove the error you have in your console*/}
 
-                        <div>
-                            <p>{post.logo.id}</p>
-                            <img src={post.logo.url} alt="logo" key={post.logo.url}></img>
-                        </div>
+            <div>
+              <p>{post.logo.id}</p>
+              <img src={post.logo.url} alt="logo" key={post.logo.url}></img>
+            </div>
 
-                  {post.photos.map((photo, index) => {
-                    return(
-                        <div key={index}>
-                        <p>{photo.id}</p>
-                        <img src={photo.url} alt="pic" width={500} key={photo.url}></img>
-                      
-                    </div>
-                    );
-                  })}
+            {post.photos.map((photo, index) => {
+              return (
+                <div key={index}>
+                  <p>{photo.id}</p>
+                  <img
+                    src={photo.url}
+                    alt="pic"
+                    width={500}
+                    key={photo.url}
+                  ></img>
+                </div>
+              );
+            })}
 
-                        
-                        <div>
-                            <p>{post.subscription.id}</p>
-                            <h2>{post.subscription.subscription_name}</h2>
-                        </div>
+            <div>
+              <p>{post.subscription.id}</p>
+              <h2>{post.subscription.subscription_name}</h2>
+            </div>
 
             <div>
               <p>{post.subscription_limite}</p>
