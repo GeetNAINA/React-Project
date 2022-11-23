382850d776bc550a5810a464def17b55200963a3
geetNaina <gp@esokia-webagency.com>
2022-11-23 15:59:25 +0400

    Commit

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,132 +1,131 @@
-import React, { useRef, useEffect, useState, useContext } from 'react';
-import AuthContext from '../../context/AuthProvider';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-
 function Login() {
-    const { setAuth } = useContext(AuthContext);
-    const userRef = useRef();
-    const errRef = useRef();
-    const [email, setEmail] = useState('');
-    const [pwd, setPwd] = useState('');
-    const [errMsg, setErrMsg] = useState('');
-    const [success, setSucess] = useState(false);
+    const initialValues = { email: '', password: '' };
+    const [formValues, setFormValues] = useState(initialValues);
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
 
-    const handleSubmit = async (e) => {
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues({ ...formValues, [name]: value }); // e.target receive the entered texts
+        setFormErrors(validate(formValues));
+    };
+    const handleSubmit = (e) => {
         e.preventDefault();
-       const PAYLOAD = 
-        {
-        identifier:email,
-        password:pwd
+        setFormErrors(validate(formValues));
+
+        const PAYLOAD = {
+            identifier: formValues.email,
+            password: formValues.password
         }
         try {
-            const response = await axios.post('https://bat-recup-staging-backend.cleverapps.io/api/users-permissions/login-annuaire', PAYLOAD)
-            setEmail('');
-            setPwd('');
-            setSucess(true);
+             axios.post('https://bat-recup-staging-backend.cleverapps.io/api/users-permissions/login-annuaire', PAYLOAD);
+            setIsSubmit(true);
         }
         catch (err) {
-
+            if (!err?.response) {
+                setFormErrors('No server Response');
+            } else if (err.response?.status === 400) {
+                setFormErrors('Missing Username or Password');
+            } else {
+                setFormErrors('Login Failed');
         }
-        
+       
     }
+}
+    
     useEffect(() => {
-        userRef.current.focus();
-    }, []);
+        console.log(formErrors);
+        // if (Object.keys(formErrors).length === 0 && isSubmit) {
+        //     console.log(formValues);
+        // }
+    });
+    const validate = (values) => {
+        const errors = {};
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        if (!values.password) {
+            errors.password = ' * Password is required!'
+        }
+        if (!values.email) {
+            errors.email = ' * Email is required!'
+            console.log(values.email)
 
-    useEffect(() => {
-        setErrMsg('');
-    }, [email, pwd]);
-
-    // const validate = (values) => {
-    //     const errors = {};
-    //     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-    //     if (!values.password) {
-    //         errors.password = ' * Password is required!'
-    //     }
-
-    //     if (!values.email) {
-    //         errors.email = ' * Email is required!'
-    //         console.log(values.email)
-
-    //     } else if (!regex.test(values.email)) {
-    //         errors.email = 'This is not a valid email!';
-    //         console.log("email is not valid")
-    //         console.log(values.email)
-
-
-    //     }
-    //     return errors;
-    // };
+        } else if (!regex.test(values.email)) {
+            errors.email = 'This is not a valid email!';
+            console.log("email is not valid")
+            console.log(values.email)
+        }
+        return errors;
+    };
     return (
         <>
-            {success ? (
-                <section>
-                    <h1>You are logged in!</h1>
-                    <br />
-                    <p>
-                        Go to Home
-                    </p>
-                </section>
-            ) : (
-                <div>
-                    <section className="gradient-custom">
-                        <div className="container py-5 h-100">
-                            <div className="row d-flex justify-content-center align-items-center h-100">
-                                <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-                                    <div className="card">
-                                        <div className="card-body p-5 text-center">
+        { formErrors.length === 0 && isSubmit? (
+            <section>
+                <h1>You are logged in!</h1>
+                <br />
+                <p>
+                    Go to Home
+                </p>
+            </section>
+        ) : (
+        <div>
+            <section className="gradient-custom">
+                <div className="container py-5 h-100">
+                    <div className="row d-flex justify-content-center align-items-center h-100">
+                        <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+                            <div className="card">
+                                <div className="card-body p-5 text-center">
 
-                                            <div className="mt-md-2 pb-5">
+                                    <div className="mt-md-2 pb-5">
 
-                                                <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
-                                                <p className="text-dark-50 mb-5">Please enter your login and password!</p>
-                                                <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-
-
-                                                <form onSubmit={handleSubmit}>
-                                                    <div className="form-floating mb-3">
-                                                        <input type="text"
-                                                            className="form-control"
-                                                            id="emailInput"
-                                                            placeholder="name@example.com"
-                                                            ref={userRef}
-                                                            autoComplete='off'
-                                                            onChange={(e) => setEmail(e.target.value)}
-                                                            value={email} />
-                                                        <label htmlFor="emailInput">Email address</label>
-                                                    </div>
-
-
-                                                    <div className="form-floating mb-3">
-                                                        <input type="password"
-                                                            onChange={(e) => setPwd(e.target.value)}
-                                                            value={pwd}
-                                                            className="form-control" id="passwordInput"
-                                                            placeholder="Password"
-                                                        />
-                                                        <label htmlFor="passwordInput">Password</label>
-                                                    </div>
-                                                    <p className="small mb-3 pb-lg-2"><a className="text-dark-50" href="#!">Forgot password?</a></p>
-
-                                                    <button className="btn btn-dark btn-lg px-5" type="submit">Login</button>
-                                                </form>
-
+                                        <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+                                        <p className="text-dark-50 mb-5">Please enter your login and password!</p>
+                                        <form onSubmit={handleSubmit}>
+                                            <div className="form-floating mb-3">
+                                                <input type="text" name="email"
+                                                    className="form-control" id="emailInput"
+                                                    placeholder="name@example.com"
+                                                    value={formValues.email}
+                                                    onChange={handleChange} />
+                                                <label htmlFor="emailInput">Email address</label>
                                             </div>
+                                            {formErrors.hasOwnProperty('email') && (
+                                                <p className="text-error">{formErrors.email}</p>
+                                            )}
 
-                                            <div>
-                                                <p className="mb-0">Don't have an account? <a href="#!" className="text-dark-50 fw-bold">Sign Up</a>
-                                                </p>
+                                            <div className="form-floating mb-3">
+                                                <input type="password" name="password"
+                                                    className="form-control" id="passwordInput"
+                                                    placeholder="Password"
+                                                    value={formValues.password}
+                                                    onChange={handleChange} />
+                                                <label htmlFor="passwordInput">Password</label>
                                             </div>
+                                            {isSubmit === true && formValues.password === '' && (
+                                                <p className="text-error">{formErrors.password}</p>
+                                            )}
 
-                                        </div>
+                                            <p className="small mb-3 pb-lg-2"><a className="text-dark-50" href="#!">Forgot password?</a></p>
+
+                                            <button className="btn btn-dark btn-lg px-5" type="submit">Login</button>
+                                        </form>
                                     </div>
+
+                                    <div>
+                                        <p className="mb-0">Don't have an account? <a href="#!" className="text-dark-50 fw-bold">Sign Up</a>
+                                        </p>
+                                    </div>
+
                                 </div>
                             </div>
                         </div>
-                    </section>
+                    </div>
                 </div>
-            )}
+            </section>
+        </div>
+        )}
         </>
     );
 }
