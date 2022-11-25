61dc780d93c28612714be2d269448f1c5baed4e4
geetNaina <gp@esokia-webagency.com>
2022-11-25 09:53:43 +0400

    Complete Correction

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
 
-function dashboard() {
+function Dashboard() {
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (!localStorage.getItem('token')) {
+      navigate('/login')
+    }
+  });
   return (
     <div>dashboard</div>
   )
 }
 
-export default dashboard
+export default Dashboard;
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
 import axios from 'axios';
+import React, { useState } from 'react';
 
 function Login() {
-    const initialValues = { email: '', password: '' };
-    const [formValues, setFormValues] = useState(initialValues);
-    const [formErrors, setFormErrors] = useState({});
-    const [isSubmit, setIsSubmit] = useState(false);
+  const initialValues = { email: '', password: '' };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormValues({ ...formValues, [name]: value }); // e.target receive the entered texts
-        setFormErrors(validate(formValues));
-    };
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setFormErrors(validate(formValues));
-            setIsSubmit(true);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value }); // e.target receive the entered texts
+    setFormErrors(validate(formValues));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
 
     const PAYLOAD = {
       identifier: formValues.email,
@@ -27,70 +27,69 @@ function Login() {
           'https://bat-recup-staging-backend.cleverapps.io/api/users-permissions/login-annuaire',
           PAYLOAD
         )
-     
-          .then((resp) => {
-            console.log(resp.data);
-            const token = resp.data.token;
-            localStorage.setItem("token", token);
-            window.location.href = '/dashboard';
-          })
-        }
-        catch(err) {
-            if (!err?.resp) {
-                setFormErrors('No server Response');
-            } else if (err.resp?.status === 400) {
-                setFormErrors('Missing Username or Password')
-            } else {
-                setFormErrors('Login Failed');
-        }
-        }
 
-        
-        }
+        .then((resp) => {
+          console.log(resp.data);
+          const token = resp.data.jwt;
+          localStorage.setItem('token', token); // are you sure that the object returned by the api has a token field? :D :P
+          window.location.href = '/dashboard'; // use the react router for the redirectio, must not forget it is that we had done
+        });
+    } catch (err) {
+      if (!err?.resp) {
+        setFormErrors('No server Response');
+      } else if (err.resp?.status === 400) {
+        setFormErrors('Missing Username or Password');
+      } else {
+        setFormErrors('Login Failed');
+      } // THE ERROR MESSAGE IS NOT DISPLAYED
+    }
+  };
 
-
-    const validate = (values) => {
-        const errors = {};
-        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-        if (!values.password) {
-            errors.password = ' * Password is required!'
-        }
-        if (!values.email) {
-            errors.email = ' * Email is required!'
-            console.log(values.email)
-
-        } else if (!regex.test(values.email)) {
-            errors.email = 'This is not a valid email!';
-            console.log("email is not valid")
-            console.log(values.email)
-        }
-        return errors;
-    };
-    return (
-
-        <div>
-            <section className="gradient-custom">
-                <div className="container py-5 h-100">
-                    <div className="row d-flex justify-content-center align-items-center h-100">
-                        <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-                            <div className="card">
-                                <div className="card-body p-5 text-center">
-
-                                    <div className="mt-md-2 pb-5">
-                                        <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
-                                        <p className="text-dark-50 mb-5">Please enter your login and password!</p>
-                                        <form onSubmit={handleSubmit}>
-                                            <div className="form-floating mb-3">
-                                                <input type="text" name="email"
-                                                    className="form-control" id="emailInput"
-                                                    placeholder="name@example.com"
-                                                    value={formValues.email}
-                                                    onChange={handleChange} />
-                                                <label htmlFor="emailInput">Email address</label>
-                                            </div>
-                                            {formErrors.hasOwnProperty('email') && (
-                                                <p className="text-error">{formErrors.email}</p>
-                                            )}
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.password) {
+      errors.password = ' * Password is required!';
+    }
+    if (!values.email) {
+      errors.email = ' * Email is required!';
+      console.log(values.email);
+    } else if (!regex.test(values.email)) {
+      errors.email = 'This is not a valid email!';
+      console.log('email is not valid');
+      console.log(values.email);
+    }
+    return errors;
+  };
+  return (
+    <div>
+      <section className="gradient-custom">
+        <div className="container py-5 h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+              <div className="card">
+                <div className="card-body p-5 text-center">
+                  <div className="mt-md-2 pb-5">
+                    <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+                    <p className="text-dark-50 mb-5">
+                      Please enter your login and password!
+                    </p>
+                    <form onSubmit={handleSubmit}>
+                      <div className="form-floating mb-3">
+                        <input
+                          type="text"
+                          name="email"
+                          className="form-control"
+                          id="emailInput"
+                          placeholder="name@example.com"
+                          value={formValues.email}
+                          onChange={handleChange}
+                        />
+                        <label htmlFor="emailInput">Email address</label>
+                      </div>
+                      {formErrors.hasOwnProperty('email') && (
+                        <p className="text-error">{formErrors.email}</p>
+                      )}
 
                       <div className="form-floating mb-3">
                         <input
